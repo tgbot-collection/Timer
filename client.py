fc1f590d3ab1589c9756ca55aa1e3da08c200c07
BennyThink <benny.think@gmail.com>
2020-11-07 00:28:16 +0800

    simple one

--- client.py
+++ client.py
@@ -7,36 +7,15 @@
 
 __author__ = "Benny <benny.think@gmail.com>"
 
-import logging
-import os
 
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from telethon import TelegramClient
+from telethon import TelegramClient, sync
 
-api_id = os.environ.get("API_ID") or 123421
-api_hash = os.environ.get("API_HASH") or '123131'
-client_ids = os.environ.get("CLIENT") or '1112'
-message = os.environ.get("MESSAGE", "🙄️")
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(filename)s [%(levelname)s]: %(message)s')
+api_id = 1
+api_hash = '2'
+client_id = '3'
+message = "时间4啊"
 
 client = TelegramClient('timer', api_id, api_hash, device_model="Timer", system_version="89", app_version="64")
 
-scheduler = AsyncIOScheduler()
-
-
-async def send_message():
-    id_list = client_ids.split(",")
-    logging.info("Sending info to %s", id_list)
-    for user in id_list:
-        entity = await client.get_entity(int(user))
-        await client.send_message(entity, message)
-
-
-scheduler.add_job(send_message, 'cron', hour='22', minute="30")
-# scheduler.add_job(send_message, 'interval', seconds=10)
-scheduler.start()
-
-if __name__ == '__main__':
-    client.start()
-    client.run_until_disconnected()
+client.start()
+client.send_message(int(client_id), message)
